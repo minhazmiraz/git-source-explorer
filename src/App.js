2b82764cf669a5bc019091d1f9df5f28c4b2b479
Md. Minhazul Islam <minhazmiraz49@gmail.com>
2021-02-09 01:32:56 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  /* fetch(
+    "https://raw.githubusercontent.com/iamshaunjp/react-context-hooks/lesson-18/booklist/public/favicon.ico"
+  )
+    .then((response) => response.text())
+    .then((res) => console.log(res)); */
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
