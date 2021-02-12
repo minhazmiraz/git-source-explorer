e3026d52fe17d5c22947542a3ba4669e099c2412
Md. Minhazul Islam <minhazmiraz49@gmail.com>
2021-02-13 02:30:07 +0600

    api call added

--- src/views/Explorer/App.js
+++ src/views/Explorer/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
   var parser = document.createElement("a");
@@ -6,20 +6,54 @@ function App() {
   var query = parser.search.substring(1);
   var repoInfo = query.split("/");
 
+  const [repoDetails, setRepoDetails] = useState("");
+  const [branches, setBranches] = useState([]);
+
   useEffect(() => {
-    fetch(
-      `https://api.github.com/repos/${repoInfo[repoInfo.length - 2]}/${
-        repoInfo[repoInfo.length - 1]
-      }/branches/master`
-    )
+    fetch("https://api.github.com/repos/octocat/Spoon-Knife")
       .then((response) => response.json())
-      .then((response) => console.log(response));
+      .then((response) => {
+        console.log("repo: ", response);
+        setRepoDetails({
+          ...repoDetails,
+          ...response,
+        });
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch("https://api.github.com/repos/octocat/Spoon-Knife/branches")
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setBranches([...branches, ...response]);
+      });
   }, []);
 
   return (
     <div className="App">
-      <p>Repo name: {repoInfo[repoInfo.length - 1]}</p>
-      <p>Owner: {repoInfo[repoInfo.length - 2]}</p>
+      <p>
+        Repository Name:{" "}
+        <b>{repoDetails ? repoDetails.name : "Loading....."}</b>
+      </p>
+      <p>
+        Created By:{" "}
+        <b>{repoDetails ? repoDetails.owner.login : "Loading...."}</b>
+      </p>
+      <select name="branch_name">
+        {!repoDetails && <option>Select a branch</option>}
+        {repoDetails &&
+          branches &&
+          branches.map((branch) =>
+            branch.name === repoDetails.default_name ? (
+              console.log("Found")
+            ) : (
+              <option key={branch.commit.sha} value={branch.name}>
+                {branch.name}
+              </option>
+            )
+          )}
+      </select>
     </div>
   );
 }
